a34ad47927ec8d2d1705408f91f7febcb6da1e67
Mariyam Ayoob <mariyam212@gmail.com>
2026-05-09 16:07:04 -0400

    Use OpenAI provider for support decisions

--- src/outcome_gated_actions/__init__.py
+++ src/outcome_gated_actions/__init__.py
@@ -1,14 +1,14 @@
 """Outcome-gated action experiment package."""
 
-from outcome_gated_actions.agent import DeterministicFakeProvider, get_provider_from_env
+from outcome_gated_actions.agent import OpenAIProvider, get_provider_from_env
 from outcome_gated_actions.judge import RubricJudge
 from outcome_gated_actions.schemas import ALLOWED_ACTIONS, AgentDecision, JudgeResult, SupportCase
 
 __all__ = [
     "ALLOWED_ACTIONS",
     "AgentDecision",
-    "DeterministicFakeProvider",
     "JudgeResult",
+    "OpenAIProvider",
     "RubricJudge",
     "SupportCase",
     "get_provider_from_env",
